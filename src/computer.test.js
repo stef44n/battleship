5b028a21358604c44d7e2fe95fd4ae7bf291afd6
Stefan <stefanseifeddine@gmail.com>
2022-12-09 22:46:24 +0000

    Update test to ensure ships are created correctly

--- src/computer.test.js
+++ src/computer.test.js
@@ -7,16 +7,17 @@ describe("computer functions and parameters", () => {
         expect(computer1.computerTurn).toBe(false);
     });
 
-    test("create only valid ships", () => {
+    test("return correct amount and collective length of ships", () => {
         let computer1 = Computer();
-        expect(computer1.createShip(3, "vertical", 3, 8)).toBe(
-            "error: out of bounds"
-        );
-        expect(computer1.createShip(3, "vertical", 3, 4)).toBe(
-            "ship placed successfully"
-        );
-        expect(computer1.createShip(3, "horizontal", 2, 5)).toBe(
-            "error: ship exists already"
-        );
+        computer1.createShips();
+        let ship1 = computer1.computerGameboard.allShipObj[0]["shipLength"];
+        let ship2 = computer1.computerGameboard.allShipObj[1]["shipLength"];
+        let ship3 = computer1.computerGameboard.allShipObj[2]["shipLength"];
+        let ship4 = computer1.computerGameboard.allShipObj[3]["shipLength"];
+        let ship5 = computer1.computerGameboard.allShipObj[4]["shipLength"];
+
+        let allLengths = ship1 + ship2 + ship3 + ship4 + ship5;
+        expect(computer1.computerGameboard.allShipObj.length).toBe(5);
+        expect(allLengths).toBe(16);
     });
 });
