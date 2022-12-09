c1511b40735e80b931840f413063e0f270ab4784
Stefan <stefanseifeddine@gmail.com>
2022-12-09 14:02:12 +0000

    Test createShip function

--- src/computer.test.js
+++ src/computer.test.js
@@ -6,4 +6,17 @@ describe("computer functions and parameters", () => {
         let computer1 = Computer();
         expect(computer1.computerTurn).toBe(false);
     });
+
+    test("create only valid ships", () => {
+        let computer1 = Computer();
+        expect(computer1.createShip(3, "vertical", 3, 8)).toBe(
+            "error: out of bounds"
+        );
+        expect(computer1.createShip(3, "vertical", 3, 4)).toBe(
+            "ship placed successfully"
+        );
+        expect(computer1.createShip(3, "horizontal", 2, 5)).toBe(
+            "error: ship exists already"
+        );
+    });
 });
