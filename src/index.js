f9a7c20c246e4e8e2b5f6519388acaff3ff70cf3
Stefan <stefanseifeddine@gmail.com>
2023-02-19 22:42:09 +0000

    Add index.js to tie all the files together

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import { cube } from "./math.js";
+import ship from "./ship.js";
+import "./style.css";
+
+import gameboard from "./gameboard.js";
+import player from "./player.js";
+import computer from "./computer.js";
+import GameLoop from "./gameloop.js";
+import createGameboards from "./DOMinteraction.js";
+
+// function component() {
+//     const element = document.createElement("pre");
+//     element.innerHTML = [
+//         "Hello webpack!MMk",
+//         // "5 cubed is equal to " + cube(5),
+//     ].join("\n\n");
+
+//     return element;
+// }
+// document.body.appendChild(component());
+
+// let ship1 = ship(3, 0, false);
+// console.log(ship1);
